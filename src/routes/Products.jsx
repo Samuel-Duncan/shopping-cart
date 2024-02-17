84642f4309a84aa22c1cd74a4f754988c6cd524a
Samuel Duncan <samuel.ira.duncan@gmail.com>
2024-02-17 17:31:53 -0500

    Fetch products from Fake Store API
    Successfully fetched an array of products ready for display in the
    Products component.

--- src/routes/Products.jsx
+++ src/routes/Products.jsx
@@ -1,8 +1,32 @@
+import { useState, useEffect } from 'react';
+
+const getProducts = () => {
+  const [products, setProducts] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products', { mode: 'cors' })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('server error');
+        }
+        return response.json();
+      })
+      .then((response) => setProducts(response))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return { products, error, loading };
+};
+
 const Products = () => {
+  const { products, error, loading } = getProducts();
   return (
-    <>
-      <h1>Explore our Collection</h1>
-    </>
+    <div className="px-4 py-2">
+      <h1 className="">Explore our Collection</h1>
+    </div>
   );
 };
 
